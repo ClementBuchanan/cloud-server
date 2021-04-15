f921ecb5f081db2218ca216304b28f6c375af6cd
Clement Buchanan <clement.buchanan@gmail.com>
2021-04-14 21:58:13 -0700

    initial commit for lab-16

---
+++ cloud-server/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let data = {};
+
+app.get('/', (req, res) => {
+  res.status(200).send(data)
+});
+
+app.post('/data', (req, res) => {
+  data = req.body;
+  res.status(200).send('added.');
+});
+app.listen(PORT, () => console.log(`You're running on ${PORT}`));
